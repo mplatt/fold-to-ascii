562ec811920be253e7779c20da34159898784083
Moritz Platt <moritz.platt@kcl.ac.uk>
2019-05-12 13:26:53 +0100

    upgrade to a modern version of QUnit

--- test/folding-tests.js
+++ test/folding-tests.js
@@ -1,20 +1,20 @@
 const ASCIIFolder = require("./../lib/ascii-folder.js");
 
-QUnit.test("replaces unknown characters", function () {
-    equal(ASCIIFolder.foldReplacing("Lorem ipsum dôlor sit amêt, pri at cetèro ëripuît inérmis.", "x"), "Lorem ipsum dolor sit amet, pri at cetero eripuit inermis.", "string with diacritics");
+QUnit.test("replaces unknown characters", assert => {
+    assert.equal(ASCIIFolder.foldReplacing("Lorem ipsum dôlor sit amêt, pri at cetèro ëripuît inérmis.", "x"), "Lorem ipsum dolor sit amet, pri at cetero eripuit inermis.", "string with diacritics");
     // "🤧😇".length == 2 (https://blog.jonnew.com/posts/poo-dot-length-equals-two)
-    equal(ASCIIFolder.foldReplacing("Lorem 🤧😇 Ipsum", "x"), "Lorem xxxx Ipsum", "string with emojis");
-    equal(ASCIIFolder.foldReplacing("お早うございます", "x"), "xxxxxxxx", "string with Kanji characters");
+    assert.equal(ASCIIFolder.foldReplacing("Lorem 🤧😇 Ipsum", "x"), "Lorem xxxx Ipsum", "string with emojis");
+    assert.equal(ASCIIFolder.foldReplacing("お早うございます", "x"), "xxxxxxxx", "string with Kanji characters");
 });
 
-QUnit.test("omits unknown characters", function () {
-    equal(ASCIIFolder.foldReplacing("Lorem ipsum dôlor sit amêt, pri at cetèro ëripuît inérmis.", ""), "Lorem ipsum dolor sit amet, pri at cetero eripuit inermis.", "string with diacritics");
-    equal(ASCIIFolder.foldReplacing("Lorem 🤧😇 Ipsum", ""), "Lorem  Ipsum", "string with emojis");
-    equal(ASCIIFolder.foldReplacing("お早うございます", ""), "", "string with Kanji characters");
+QUnit.test("omits unknown characters", assert => {
+    assert.equal(ASCIIFolder.foldReplacing("Lorem ipsum dôlor sit amêt, pri at cetèro ëripuît inérmis.", ""), "Lorem ipsum dolor sit amet, pri at cetero eripuit inermis.", "string with diacritics");
+    assert.equal(ASCIIFolder.foldReplacing("Lorem 🤧😇 Ipsum", ""), "Lorem  Ipsum", "string with emojis");
+    assert.equal(ASCIIFolder.foldReplacing("お早うございます", ""), "", "string with Kanji characters");
 });
 
-QUnit.test("maintains unknown characters", function () {
-    equal(ASCIIFolder.foldMaintaining("Lorem ipsum dôlor sit amêt, pri at cetèro ëripuît inérmis."), "Lorem ipsum dolor sit amet, pri at cetero eripuit inermis.", "string with diacritics");
-    equal(ASCIIFolder.foldMaintaining("Lorem 🤧😇 Ipsum"), "Lorem 🤧😇 Ipsum", "string with emojis");
-    equal(ASCIIFolder.foldMaintaining("お早うございます"), "お早うございます", "string with Kanji characters");
+QUnit.test("maintains unknown characters", assert => {
+    assert.equal(ASCIIFolder.foldMaintaining("Lorem ipsum dôlor sit amêt, pri at cetèro ëripuît inérmis."), "Lorem ipsum dolor sit amet, pri at cetero eripuit inermis.", "string with diacritics");
+    assert.equal(ASCIIFolder.foldMaintaining("Lorem 🤧😇 Ipsum"), "Lorem 🤧😇 Ipsum", "string with emojis");
+    assert.equal(ASCIIFolder.foldMaintaining("お早うございます"), "お早うございます", "string with Kanji characters");
 });
